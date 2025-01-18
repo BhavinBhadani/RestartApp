a74825475c6c8b00800073d0170c3af14d05f8e5
Bhavin B <bhavinbhadani@yahoo.in>
2025-01-18 10:57:32 +0530

    Initial Commit

---
+++ RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Bhavin Bhadani on 18/01/25.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
